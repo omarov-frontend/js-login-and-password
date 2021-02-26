c7233f46d4a3a420951a492472adf8181b489895
Shamil Omarov <s_omarov05@mail.ru>
2021-02-26 19:55:52 +0300

    Поправил ревью

--- js/main.js
+++ js/main.js
@@ -3,11 +3,9 @@ const passwordInput = document.querySelector('#password');
 const button = document.querySelector('#button');
 
 button.addEventListener('click', function() {
-    if(loginInput.value === 'Ivan' && passwordInput.value === '334455') {
-        alert('Добро пожаловать!');
-    } else if(loginInput.value === 'Alex' && passwordInput.value === '777') {
-        alert('Добро пожаловать!');
-    } else if(loginInput.value === 'Petr' && passwordInput.value === 'p1234') {
+    if(loginInput.value === 'Ivan' && passwordInput.value === '334455' ||
+        loginInput.value === 'Alex' && passwordInput.value === '777' ||
+        loginInput.value === 'Petr' && passwordInput.value === 'p1234') {
         alert('Добро пожаловать!');
     } else {
         alert('Ошибка входа!');
